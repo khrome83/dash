02f436710104e9a7298e7c1d6897e4f55cf3aa06
xpyxel <xpyxel@gmail.com>
2020-04-15 20:53:41 -0400

    Basic set/get functionality with limit

--- mod.ts
+++ mod.ts
@@ -1,13 +1,26 @@
-interface CacheEntry {
-  identifier: string | number;
-  data: Uint8Array;
+interface CacheEntry<type> {
+  key: string | number;
+  data: type;
 }
+
+type Nullable<T> = T | null;
 
 class Cache {
   private limit: number;
-  private entries: CacheEntry[];
+  private entries: CacheEntry<any>[];
   constructor(cacheLimit: number) {
     this.limit = cacheLimit;
     this.entries = [];
+  }
+  set<T>(key: string | number, data: T): void {
+    const entry: CacheEntry<T> = { key, data };
+    if (this.entries.length >= this.limit) {
+      this.entries.shift();
+      this.entries.push(entry);
+    } else this.entries.push(entry);
+  }
+  get(key: string | number): Nullable<any> {
+    const entry = this.entries.find((e) => e.key == key);
+    return entry?.data ?? null;
   }
 }
