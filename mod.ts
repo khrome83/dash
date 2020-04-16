e4ca1d175d0c34e8e9597176a916899db64c587b
xpyxel <xpyxel@gmail.com>
2020-04-16 10:08:47 -0400

    Create Identifier type for cleanliness

--- mod.ts
+++ mod.ts
@@ -1,19 +1,20 @@
 type Nullable<T> = T | null;
+type Identifier = string | number;
 
 export class Cache {
   #limit: number;
-  #entries: Map<string | number, any>;
+  #entries: Map<Identifier, any>;
   constructor(cacheLimit: number) {
     this.#limit = cacheLimit;
     this.#entries = new Map();
   }
-  set(key: string | number, data: any): void {
+  set(key: Identifier, data: any): void {
     if (this.#entries.size >= this.#limit) {
       this.#entries.delete(this.#entries.keys().next().value);
       this.#entries.set(key, data);
     } else this.#entries.set(key, data);
   }
-  get(key: string | number): Nullable<any> {
+  get(key: Identifier): Nullable<any> {
     if (this.#entries.has(key)) {
       this.#entries.set(key, this.#entries.get(key));
       return this.#entries.get(key);
