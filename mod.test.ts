3f623f8e032117dd84ef6aa23062bf70799a97b7
xpyxel <xpyxel@gmail.com>
2020-04-17 11:53:57 -0400

    Add tests for serialized cache

--- mod.test.ts
+++ mod.test.ts
@@ -5,13 +5,21 @@ import {
   assertNotEquals,
 } from "https://deno.land/std@v0.41.0/testing/asserts.ts";
 
-const cache = new Cache(100000);
+const ncache = new Cache({
+  limit: 100000,
+  serialize: false,
+});
 
-function insertItems(count: number) {
+const scache = new Cache({
+  limit: 100000,
+  serialize: true,
+});
+
+function insertItems(cache: Cache, count: number) {
   for (let i = 0; i < count; i++) cache.set(i, { d: i });
 }
 
-function getItems(count: number) {
+function getItems(cache: Cache, count: number) {
   for (let i = 0; i < count; i++) {
     const value = cache.get(i);
     assertNotEquals(value, null, "The returned cache item is null");
@@ -19,21 +27,53 @@ function getItems(count: number) {
   }
 }
 
-Deno.test("Insert 100 Items", () => insertItems(100));
-Deno.test("Get 100 Items", () => getItems(100));
+// Unserialized Cache
 
-Deno.test("Insert 1000 Items", () => insertItems(1000));
-Deno.test("Get 1000 Items", () => getItems(1000));
+Deno.test("Insert 100 Items (Unserialized)", () => insertItems(ncache, 100));
+Deno.test("Get 100 Items (Unserialized)", () => getItems(ncache, 100));
 
-Deno.test("Insert 10000 Items", () => insertItems(10000));
-Deno.test("Get 10000 Items", () => getItems(10000));
+Deno.test("Insert 1000 Items (Unserialized)", () => insertItems(ncache, 1000));
+Deno.test("Get 1000 Items (Unserialized)", () => getItems(ncache, 1000));
 
-Deno.test("Insert 100000 Items", () => insertItems(100000));
-Deno.test("Get 100000 Items", () => getItems(100000));
+Deno.test("Insert 10000 Items (Unserialized)", () =>
+  insertItems(ncache, 10000));
 
-Deno.test("Check For LRU Item Deletion", () => {
-  insertItems(100001);
-  assertEquals(cache.get(0), null, "Least frequently used item not deleted");
+Deno.test("Get 10000 Items (Unserialized)", () => getItems(ncache, 10000));
+
+Deno.test("Insert 100000 Items (Unserialized)", () =>
+  insertItems(ncache, 100000));
+
+Deno.test("Get 100000 Items (Unserialized)", () => getItems(ncache, 100000));
+
+Deno.test("Check For LRU Item Deletion (Unserialized)", () => {
+  insertItems(ncache, 100001);
+  assertEquals(ncache.get(0), null, "Least frequently used item not deleted");
 });
 
-Deno.test("Overflow Cache Limit ", () => insertItems(200000));
+Deno.test("Overflow Cache Limit (Unserialized) ", () =>
+  insertItems(ncache, 200000));
+
+// Serialized Cache
+
+Deno.test("Insert 100 Items (Serialized)", () => insertItems(scache, 100));
+Deno.test("Get 100 Items (Serialized)", () => getItems(scache, 100));
+
+Deno.test("Insert 1000 Items (Serialized)", () => insertItems(scache, 1000));
+Deno.test("Get 1000 Items (Serialized)", () => getItems(scache, 1000));
+
+Deno.test("Insert 10000 Items (Serialized)", () => insertItems(scache, 10000));
+
+Deno.test("Get 10000 Items (Serialized)", () => getItems(scache, 10000));
+
+Deno.test("Insert 100000 Items (Serialized)", () =>
+  insertItems(scache, 100000));
+
+Deno.test("Get 100000 Items (Serialized)", () => getItems(scache, 100000));
+
+Deno.test("Check For LRU Item Deletion (Serialized)", () => {
+  insertItems(scache, 100001);
+  assertEquals(scache.get(0), null, "Least frequently used item not deleted");
+});
+
+Deno.test("Overflow Cache Limit (Serialized) ", () =>
+  insertItems(scache, 200000));
